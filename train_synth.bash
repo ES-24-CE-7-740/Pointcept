fce8858b7a6ef40428e4a2b937485ce9f4507584
Andreas Højrup <93713572+andreasHovaldt@users.noreply.github.com>
2024-12-09 10:45:31 +0100

    Update train_synth.bash

--- train_synth.bash
+++ train_synth.bash
@@ -1,3 +1,3 @@
 export PYTHONPATH=./
 
-python tools/train.py --config-file ./configs/ttgch_with_model_labels/real/semseg-oacnns-v1m1-0-base.py --num-gpus 2 --options save_path=exp/tgc_synth_oacnns
+python tools/train.py --config-file ./configs/tractors_and_combines_synth/semseg-oacnns-v1m1-0-base.py --num-gpus 2 --options save_path=exp/tgc_synth_oacnns
